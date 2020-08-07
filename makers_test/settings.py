93874e8323d7723dfdfcadaf58dd752293ebb9a1
admin <admin@admin.com>
2020-08-07 11:12:45 +0600

    Add permission

--- makers_test/settings.py
+++ makers_test/settings.py
@@ -144,5 +144,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
-LOGIN_URL = 'accounts/login/'
+LOGIN_URL = '/accounts/login/'
 LOGOUT_REDIRECT_URL = '/'
